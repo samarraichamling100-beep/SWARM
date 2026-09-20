2677fc24bcac00cecd2e4d46d175a3f60634305e
samarraichamling100-beep <samarraichamling100@gmail.com>
2026-09-20 09:35:23 +0545

    the player bullet

--- src/playerbullet.cpp
+++ src/playerbullet.cpp
@@ -3,10 +3,14 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+
 struct Bullet
 {
+    //The bullet structure
     Rectangle Rect = {0,0,5,10};
+    //speed
     float Speed = 600.0f;
+    //direction
     Vector2 Direction;
 };
 
@@ -20,6 +24,7 @@ class PlayerBullet
 
     void Draw_PlayerBullet()
     {
+        //Drawing all the bullets in bulletList
         for (auto & bullet : BulletList)
         {
             DrawRectangleRec(bullet.Rect,YELLOW);
@@ -32,6 +37,7 @@ class PlayerBullet
             Vector2 mousePosition = GetMousePosition();
 
             // Ship center
+            //Using the hitbox since the shipRect is too big
             Vector2 shipPosition =
             {
                 Hitbox.x + Hitbox.width / 4.5f,
@@ -39,28 +45,30 @@ class PlayerBullet
             };
 
             // Mouse - ship = direction from ship to mouse
+            //directly did this because it is eaiser and creating other variable was unclean
             Vector2 Direction =
             {
                 mousePosition.x - shipPosition.x,
                 mousePosition.y - shipPosition.y
             };
-
+            //Normalizing
             Direction = Vector2Normalize(Direction);
-
+            //Creating new bullet to push
             Bullet newBullet;
-
+            //StartingPos of new bullet
             newBullet.Rect.x = shipPosition.x;
             newBullet.Rect.y = shipPosition.y;
-
+            //Applying the direction to new bullet
             newBullet.Direction = Direction;
-
+            //Pushing back the new bullet to the bullet list
             BulletList.push_back(newBullet);
         }
     }
     void Move_PlayerBullet()
     {
+        //delta time
         float dt = GetFrameTime();
-
+        //We multiply the direction with speed * dt to get the speed to the mouse_position point when shooting
         for (auto &bullet : BulletList)
         {
             bullet.Rect.x += bullet.Direction.x * bullet.Speed * dt;
@@ -69,7 +77,8 @@ class PlayerBullet
     }
     void delete_unnecessary_bullet()
     {
-
+        //Deleting unnecssaray bullets (aka the bullets which are outside the screen)
+        //A cool lambda funtion 
         std::erase_if(BulletList,[](auto &bullet)
         {
             return 
