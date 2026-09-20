ad0fcaaf7e6508adaeefe099c2a5bbfd16c4d1b2
samarraichamling100-beep <samarraichamling100@gmail.com>
2026-09-20 09:43:01 +0545

    The testing

--- src/test.cpp
+++ src/test.cpp
@@ -5,13 +5,8 @@
 
 int main ()
 {
+    //Testing if the list size starts from 0 or 1
     std::vector<int> List = {0,1,2,4,4,5};
 
     std::cout << "List Size : " << List.size() << std::endl;
-
-    float a = 3;
-    float b = 3;
-
-    const float Y = a + b;
-    std::cout << Y << std::endl;
 }
